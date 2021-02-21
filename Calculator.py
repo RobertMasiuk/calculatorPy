71beeaa3bd9c9935de75b39bbd449679598c662a
Robert Masiuk <robert@masiuk.pl>
2021-02-21 17:33:35 +0100

    4 simple opration

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,24 @@
 # first program
 print("this is simple calculator")
+'''
 fist = int(input("give first number"))
 second =  int(input("give socond number"))
 print(fist + second)
 
+'''
+
+mathOperation = input("input type of math operation")
+
+fist = int(input("enter first number"))
+second =  int(input("enter socond number"))
+
+if mathOperation == '+':
+    print(fist + second)
+if mathOperation == '-':
+    print(fist - second)
+if mathOperation == '*':
+    print(fist * second)
+if mathOperation == '/' and second !=0:
+    print(fist / second)
+else:
+    print("you can't divide by '0'")
